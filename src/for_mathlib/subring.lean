6e9928219498a348c5566e29fece02d43a95de60
Johan Commelin <johan@commelin.net>
2018-06-08 16:02:08 +0200

    Integrally closed subrings

--- src/for_mathlib/subring.lean
+++ src/for_mathlib/subring.lean
@@ -1,21 +1,43 @@
 import for_mathlib.add_subgroup group_theory.submonoid
+import poly
+import algebra.ring
+local attribute [instance] classical.prop_decidable
 
 open group
 
 variables {R : Type} [ring R]
 
-/-- `S` is a subgroup: a set containing 1 and closed under multiplication, addition and and additive inverse. -/
+/-- `S` is a subring: a set containing 1 and closed under multiplication, addition and and additive inverse. -/
 class is_subring  (S : set R) extends is_add_subgroup S, is_submonoid S : Prop.
 
-instance subtype.ring {S : set R} [is_subring S] : ring S :=
+instance subset.ring {S : set R} [is_subring S] : ring S :=
 { add_comm      := assume ⟨a,_⟩ ⟨b,_⟩, subtype.eq $ add_comm _ _,
   left_distrib  := assume ⟨a,_⟩ ⟨b,_⟩ ⟨c,_⟩, subtype.eq $ left_distrib _ _ _,
   right_distrib := assume ⟨a,_⟩ ⟨b,_⟩ ⟨c,_⟩, subtype.eq $ right_distrib _ _ _,
   .. subtype.add_group,
   .. subtype.monoid }
 
-def is_integral (S : set R) [is_subring S] (r : R) : Prop := sorry
--- ∃ f : (poly S), (is_monic f) ∧ (f(r) = 0)
+instance subtype.ring {S : set R} [is_subring S] : ring (subtype S) :=
+{ add_comm      := assume ⟨a,_⟩ ⟨b,_⟩, subtype.eq $ add_comm _ _,
+  left_distrib  := assume ⟨a,_⟩ ⟨b,_⟩ ⟨c,_⟩, subtype.eq $ left_distrib _ _ _,
+  right_distrib := assume ⟨a,_⟩ ⟨b,_⟩ ⟨c,_⟩, subtype.eq $ right_distrib _ _ _,
+  .. subtype.add_group,
+  .. subtype.monoid }
+
+namespace is_ring_hom
+
+instance {S : set R} [is_subring S] : is_ring_hom (@subtype.val R S) :=
+{ map_add := λ _ _, rfl,
+  map_mul := λ _ _, rfl,
+  map_one := rfl }
+
+end is_ring_hom
+
+noncomputable def poly.map {S : Type} [ring S] (f : S → R) [is_ring_hom f] : polynomial S → polynomial R :=
+finsupp.map_range f (is_ring_hom.map_zero f)
+
+def is_integral (S : set R) [is_subring S] (r : R) : Prop :=
+∃ f : polynomial S, (polynomial.monic f) ∧ (poly.map (@subtype.val R S) f).eval r = 0
 
 def is_integrally_closed (S : set R) [is_subring S] :=
 ∀ r : R, (is_integral S r) → r ∈ S
