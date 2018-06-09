60eff4d3bc38e60d5ff2c10336b321f8ed36c9ad
Patrick Massot <patrickmassot@free.fr>
2018-06-10 00:06:30 +0200

    Uniform structure on top rings

--- src/adic_space.lean
+++ src/adic_space.lean
@@ -17,12 +17,19 @@ end comm_ring
 
 section topological_ring
 
--- The following two lines are a nasty hack to switch between having the carrier 
--- Type implicit or explicit.
-variables (R : Type) [comm_ring R] [topological_space R] [comm_ring R] [topological_ring R]  
-variables {A : Type} [comm_ring A] [topological_space A] [comm_ring A] [topological_ring A]  
+variables (R : Type*) [comm_ring R] [topological_space R] [comm_ring R] [topological_ring R]  
 
 def nhd_zero := (nhds (0 : R)).sets
+variable {R}
+lemma nhd_zero_symmetric {V : set R} : V ∈ nhd_zero R →  (λ a, -a) '' V ∈ nhd_zero R :=
+begin
+  intro H,
+  have := continuous.tendsto (topological_add_group.continuous_neg R) 0,
+  rw (show (λ (a : R), -a) 0 = 0, by simp) at this,
+  have almost:= this H,
+  have aux : { r : R | -r ∈ V } = (λ a, -a) '' V, by simp[set.image, neg_eq_iff_neg_eq],
+  simpa [filter.mem_map, aux] using almost
+end
 
 -- peredicates we need for topological rings
 
@@ -31,24 +38,80 @@ def nhd_zero := (nhds (0 : R)).sets
 definition is_hausdorff (α : Type*) [topological_space α] : Prop := 
   ∀x y, x ≠ y → ∃u v : set α, is_open u ∧ is_open v ∧ x ∈ u ∧ y ∈ v ∧ u ∩ v = ∅
 
-open filter
-instance toplogical_ring.to_uniform_space : uniform_space R := 
-uniform_space.of_core { uniformity  := (⨅ U ∈ nhd_zero R, principal {p : R×R | p.2 - p.1 ∈ U}),
-                        refl        := begin simp, intros i H r, exact mem_of_nhds H end,
-                        symm        := sorry,
-                        comp        := sorry }
 
+open filter lattice
+lemma half_nhd {α : Type*} [topological_space α] [add_group α] [topological_add_group α] (U ∈ (nhds (0 : α)).sets) : 
+  ∃ V ∈ (nhds (0 : α)).sets, ∀ v w ∈ V, v + w ∈ U :=
+/- Here is the story : by continuity of addition, and because 0 + 0 = 0,
+   (+)⁻¹(U) is a neighborhood of (0, 0). Hence it contains some V₁ × V₂. Then set V = V₁ ∩ V₂ -/
+begin
+  have nhdb_in_prod : ((λ a : α × α, a.1+a.2)⁻¹' U) ∈ (nhds ((0, 0) : α × α)).sets,
+    by apply tendsto_add' ; simp [H],
+  rw nhds_prod_eq at nhdb_in_prod,
+  rcases mem_prod_iff.1 nhdb_in_prod with ⟨V₁, H₁, V₂, H₂, H⟩,
+  have H12: V₁ ∩ V₂ ∈ (nhds (0 : α)).sets := inter_mem_sets H₁ H₂,    
+  existsi [(V₁ ∩ V₂), H12],
+  intros v w Hv Hw,
+  have : (v,w) ∈ set.prod V₁ V₂, by finish,
+  simpa using H this
+end
+
+
+instance toplogical_ring.to_uniform_space : uniform_space R := 
+uniform_space.of_core { 
+  uniformity  := (⨅ U ∈ nhd_zero R, principal {p : R×R | p.2 - p.1 ∈ U}),
+  refl        := begin simp, intros i H r, exact mem_of_nhds H end,
+  symm        := tendsto_infi.2 $ assume V, tendsto_infi.2 $ assume h,
+    tendsto_infi' ((λ r, -r) '' V)
+      begin 
+        simp [nhd_zero_symmetric h],
+        unfold tendsto,
+        rw map_principal,
+        have H : prod.swap '' {p : R × R | ∃ (x : R), x ∈ V ∧ -x = p.snd + -p.fst} = {p : R × R | p.snd - p.fst ∈ V},
+        begin 
+          rw set.image_swap_eq_preimage_swap,
+          ext p,
+          cases p with r1 r2,
+          change (∃ x, x ∈ V ∧ -x = r1 - r2) ↔ (r2 - r1 ∈ V),
+          have : ∀ x, -x = r1 - r2 ↔ x = r2 - r1,
+            by intro x; rw [neg_eq_iff_neg_eq, eq_comm]; simp,
+          simp only [this], simp
+      end,
+      simp [H],                           
+    end,
+  comp        := le_infi $ assume V, le_infi $ assume H, 
+    begin
+      rcases half_nhd V H with ⟨U, ⟨h, h'⟩⟩,
+      have comprel : comp_rel {p : R × R | p.snd - p.fst ∈ U} {p : R × R | p.snd - p.fst ∈ U} ⊆ {p : R × R | p.snd - p.fst ∈ V},
+      begin
+        intros p p_comp_rel,
+        rcases p_comp_rel with ⟨z, ⟨Hz1, Hz2⟩⟩,
+        simpa using h' _ _ Hz1 Hz2,
+      end,
+      replace comprel : principal (comp_rel {p : R × R | p.snd - p.fst ∈ U} {p : R × R | p.snd - p.fst ∈ U}) ≤ principal {p : R × R | p.snd - p.fst ∈ V},
+        simpa using comprel,
+      apply lift'_le,
+      swap,
+      exact comprel,
+      apply mem_infi_sets U,
+      apply mem_infi_sets h,
+      simp
+    end }
+
+variable (R)
 /-- Wedhorn Definition 5.31 page 38 -/
 definition is_complete : Prop := is_hausdorff R ∧ ∀ {f:filter R}, cauchy f → ∃x, f ≤ nhds x
 
+variable {R}
 /-- Wedhorn Definition 5.27 page 36 -/
 definition is_bounded 
-  (B : set A) : Prop := ∀ U ∈ nhd_zero A, ∃ V ∈ nhd_zero A, ∀ v ∈ V, ∀ b ∈ B, v*b ∈ U
+  (B : set R) : Prop := ∀ U ∈ nhd_zero R, ∃ V ∈ nhd_zero R, ∀ v ∈ V, ∀ b ∈ B, v*b ∈ U
 
-def power_set (r : A) : set A := set.range (λ n : ℕ, r^n)
+def power_set (r : R) : set R := set.range (λ n : ℕ, r^n)
 
-definition is_power_bounded (r : A) : Prop := is_bounded (power_set r)
+definition is_power_bounded (r : R) : Prop := is_bounded (power_set r)
 
+variable (R)
 -- Somehow we need R° both as a subset of R and a subtype. 
 -- There is a coercion from the set to the subtype but relying naively on it seems to bring 
 -- type class resolution issues
@@ -63,12 +126,14 @@ instance : topological_ring (power_bounded_subring R) := sorry
 definition is_uniform : Prop := is_bounded (power_bounded_subring_set R)
 
 theorem p_is_power_bounded [p : Prime] : is_power_bounded (p : power_bounded_subring R) := sorry
-definition is_pseudo_uniformizer : A → Prop := sorry
+
+variable {R}
+definition is_pseudo_uniformizer : R → Prop := sorry
 end topological_ring
 
 -- Schol= : "Recall that a topological ring R is Tate if it contains an
 -- open and bounded subring R0 ⊂ R and a topologically nilpotent unit pi ∈ R; such elements are
--- called pseudo-uniformizers.""
+-- called pseudo-uni7formizers.""
 -- we need definitions of bounded subsete and topologically nilpotent -- and do we have unit? Probably.
 class Tate_ring (R : Type) extends comm_ring R, topological_space R, topological_ring R :=
 (unfinished : sorry)
