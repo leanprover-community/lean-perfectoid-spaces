e70cf3c681268ffad4c902bfe5ca7ee079e6fb49
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-06-24 22:29:21 +0100

    adding Spv (in two ways -- one using quotient and one using induced inequalities)

---
+++ src/zfc_valuation_spectrum.lean
@@ -0,0 +1,18 @@
+import valuation_spectrum data.equiv
+
+definition zfc.Spv (A : Type) [comm_ring A] : Type := 
+  {ineq : A → A → Prop // ∃ v : valuations A, ∀ r s : A, ineq r s ↔ v.f r ≤ v.f s}
+
+variables {A : Type} [comm_ring A]
+
+--set_option pp.implicit true 
+--set_option pp.notation false
+def to_fun (A : Type) [comm_ring A] := quotient.lift (λ v,(⟨λ r s, valuations.f v r ≤ valuations.f v s,⟨v,λ r s,iff.rfl⟩⟩ : zfc.Spv A)) 
+      (λ v w H,subtype.eq $ funext $ λ r, funext $ λ s, propext $ H r s)
+
+theorem to_fun.bijective : function.bijective (to_fun A) := ⟨
+  λ vs ws,quotient.induction_on₂ vs ws $ λ v w H,quotient.sound $ λ r s,iff_of_eq $ congr_fun (congr_fun (subtype.mk.inj H) r) s--(congr_fun (congr_fun (subtype.mk.inj H) r) s)))
+    ,λ ⟨ineq,⟨v,Hv⟩⟩,⟨⟦v⟧,subtype.eq $ funext $ λ r,funext $ λ s,propext (Hv r s).symm⟩⟩
+
+noncomputable lemma Spv_equiv (A : Type) [comm_ring A] : equiv (Spv A) (zfc.Spv A) :=
+  equiv.of_bijective (to_fun.bijective)
