780128024542015c8d967c7e97df23872eb0c35f
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-07-19 10:53:13 +0100

    tidy

--- src/valuation_spectrum.lean
+++ src/valuation_spectrum.lean
@@ -1,12 +1,13 @@
---import data.equiv.basic
 import valuations 
 import analysis.topology.topological_space
+
 universes u v
+
 open is_valuation 
 
-definition Spv (A : Type u) [comm_ring A] := 
-{ineq : A → A → Prop // ∃ (Γ : Type u) [linear_ordered_comm_group Γ],
-  by exactI ∃ (v : valuation A Γ), ∀ r s : A, ineq r s ↔ v r ≤ v s}
+definition Spv (R : Type u) [comm_ring R] := 
+{ineq : R → R → Prop // ∃ (Γ : Type u) [linear_ordered_comm_group Γ],
+  by exactI ∃ (v : valuation R Γ), ∀ r s : R, ineq r s ↔ v r ≤ v s}
 
 -- TODO (if I understood Mario correctly):
 -- definition Spv.mk (A : Type u) [comm_ring A] (Γ : Type v) -- note : not type u 
@@ -18,16 +19,9 @@ namespace Spv
 variables {A : Type*} [comm_ring A]
 
 definition basic_open (r s : A) : set (Spv A) := 
-  {v | v.val r s ∧ ¬ v.val s 0}
-
---  quotient.lift (λ v, valuations.f v r ≤ v.f s ∧ v.f s > 0) (λ v w H,propext begin 
---  dsimp,
---  rw basic_open.aux1 r s v w H,
---  rw basic_open.aux2 s v w H,
---  end)
---  (λ v w H, show (v(r) ≤ v(s) ∧ v(s) > 0) ↔ (w(r) ≤ w(s) ∧ w(s) > 0) _
+{v | v.val r s ∧ ¬ v.val s 0}
 
 instance (A : Type*) [comm_ring A] : topological_space (Spv A) :=
-  topological_space.generate_from {U : set (Spv A) | ∃ r s : A, U = basic_open r s}
+topological_space.generate_from {U : set (Spv A) | ∃ r s : A, U = basic_open r s}
 
 end Spv 
