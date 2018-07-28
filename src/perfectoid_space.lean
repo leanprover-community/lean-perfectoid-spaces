60bbc541fa6172bbfe13535042d2466dc5b5747a
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-07-28 12:57:49 +0100

    fixing universes

--- src/perfectoid_space.lean
+++ src/perfectoid_space.lean
@@ -8,12 +8,12 @@ open nat.Prime
 variable [nat.Prime] -- fix a prime p
 
 /-- A perfectoid ring, following Fontaine Sem Bourb-/
-class perfectoid_ring (R : Type) extends Tate_ring R :=
+class perfectoid_ring (R : Type*) extends Tate_ring R :=
 (complete : is_complete R)
 (uniform  : is_uniform R)
 (ramified : ∃ ϖ : Rᵒ, (is_pseudo_uniformizer ϖ) ∧ (ϖ^p ∣ p))
 (Frob     : ∀ a : Rᵒ, ∃ b : Rᵒ, (p : Rᵒ) ∣ (b^p - a))
 
-class perfectoid_space (X : Type) extends adic_space X :=
-(perfectoid_cover : ∀ x : X, ∃ (U : opens X) (A : Huber_pair) [perfectoid_ring A],
+class perfectoid_space (X : Type*) extends adic_space X :=
+(perfectoid_cover : ∀ x : X, ∃ (U : opens X) (A : Huber_pair) [perfectoid_ring A.R],
   (x ∈ U) ∧ is_preadic_space_equiv U (Spa A))
