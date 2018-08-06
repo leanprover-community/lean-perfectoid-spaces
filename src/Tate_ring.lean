0e683fd0c3cd4f0b9af730d14c4c5676d642d800
Johan Commelin <johan@commelin.net>
2018-08-06 07:55:56 +0200

    Definition of Tate ring

--- src/Tate_ring.lean
+++ src/Tate_ring.lean
@@ -1,10 +1,22 @@
 import analysis.topology.topological_structures
+import for_mathlib.subring
 
 -- Scholze : "Recall that a topological ring R is Tate if it contains an
--- open and bounded subring R0 ⊂ R and a topologically nilpotent unit pi ∈ R; such elements are
--- called pseudo-uniformizers.""
--- we need definitions of bounded subsete and topologically nilpotent -- and do we have unit? Probably.
+-- open and bounded subring R₀ ⊂ R and a topologically nilpotent unit ϖ ∈ R; such elements are
+-- called pseudo-uniformizers."
+
+variables {R : Type*} [comm_ring R] [topological_space R] [topological_ring R]
+
+def topologically_nilpotent (r : R) : Prop :=
+∀ U ∈ (nhds (0 :R)).sets, ∃ N : ℕ, ∀ n : ℕ, n > N → r^n ∈ U
+
+definition is_pseudo_uniformizer (ϖ : units R) : Prop := topologically_nilpotent ϖ.val
+
 class Tate_ring (R : Type*) extends comm_ring R, topological_space R, topological_ring R :=
-(unfinished2 : sorry)
+(R₀ : set R)
+(R₀_is_open : is_open R₀)
+(R₀_is_subring : is_subring R₀)
+(ϖ : units R)
+(ϖ_is_pseudo_uniformizer : is_pseudo_uniformizer ϖ)
 
 -- need an instance from Tate to Huber
