283d3a2f8dac159d65d061a0d24c595c2b099c2f
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-09-25 08:37:44 +0100

    termifying a proof for some reason

--- src/Spa.lean
+++ src/Spa.lean
@@ -5,6 +5,7 @@ import Huber_pair
 -- Wedhorn def 7.23.
 definition Spa (A : Huber_pair) := {vs : Spv A.R // Spv.is_continuous vs ∧ ∀ r : A.R, r ∈ A.Rplus → vs.val r 1}
 
+/-- basic open corresponding to r, s is v : v(r) <= v(s) and v(s) isn't 0 ( = v(0) ) -/
 definition basic_open {A : Huber_pair} (r s : A.R) : set (Spa A) := 
 {vs | vs.val.val r s ∧ ¬ vs.val.val s 0}
 
@@ -15,27 +16,12 @@ topological_space.generate_from {U : set (Spa A) | ∃ r s : A.R, U = basic_open
 definition rational_open {A : Huber_pair} (s : A.R) (T : set A.R) : set (Spa A) :=
 {vs | (∀ t ∈ T, (vs.val.val t s)) ∧ (¬ vs.val.val s 0)}
 
-definition rational_open_Inter {A : Huber_pair} (s : A.R) (T : set A.R) :
+theorem rational_open_Inter {A : Huber_pair} (s : A.R) (T : set A.R) :
 rational_open s T = (set.Inter (λ (t : T), basic_open t s)) ∩ {vs | ¬ vs.val.val s 0} :=
-begin
-  apply set.ext,
-  intro vs,
-  split,
-    intro H,
-    split,
-      rw set.mem_Inter,intro t,
-      split,
-        exact H.left t.val t.property,
-      exact H.right,
-    exact H.right,
-  intro H,
-  cases H with H1 H2,
-  rw set.mem_Inter at H1,
-  split,
-    intros t ht,
-    exact (H1 ⟨t,ht⟩).left,
-  exact H2
-end
+set.ext $ λ vs, ⟨λ H, ⟨set.mem_Inter.2 $ λ t,⟨H.left _ t.property,H.right⟩,H.right⟩,
+  λ ⟨H1,H2⟩,⟨λ t ht,(set.mem_Inter.1 H1 ⟨t, ht⟩).1,H2⟩⟩
+
+#check rational_open_Inter
 
 lemma rational_open_is_open {A : Huber_pair} (s : A.R) (T : set A.R) (HFinT : fintype T) :
 is_open (rational_open s T) := sorry -- finite intersection of opens is open
