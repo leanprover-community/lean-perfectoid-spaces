d1e2db0f893e4def5292de532b22a73888a3d881
Patrick Massot <patrickmassot@free.fr>
2019-03-16 21:01:02 +0100

    Update mathlib and fix build

--- src/for_mathlib/topology.lean
+++ src/for_mathlib/topology.lean
@@ -9,10 +9,3 @@ open topological_space
 -- We need to think whether we could directly use the class t2_space (which is not using opens though)
 definition is_hausdorff (α : Type*) [topological_space α] : Prop :=
   ∀ x y, x ≠ y → ∃ u v : opens α, x ∈ u ∧ y ∈ v ∧ u ∩ v = ∅
-
-@[to_additive topological_add_group.ext, extensionality]
-lemma topological_group.ext {G : Type*} [group G] (t t' : topological_space G)
-  (tg : @topological_group G t _) (tg' : @topological_group G t' _)
-  (h : @nhds G t 1 = @nhds G t' 1) : t = t' :=
-eq_of_nhds_eq_nhds $ λ x, by
-  rw [← @nhds_translation_mul_inv G t _ _ x , ← @nhds_translation_mul_inv G t' _ _ x , ← h]
