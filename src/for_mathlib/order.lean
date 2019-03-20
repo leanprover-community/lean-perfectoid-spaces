26a27182c009efa9fcb09b4abe97cd48ca241011
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2019-03-20 19:13:43 +0000

    more comap -> lift

--- src/for_mathlib/order.lean
+++ src/for_mathlib/order.lean
@@ -1,12 +1,14 @@
+import order.basic
 namespace preorder
 
 variables {α : Type*} {β : Type*}
 
-def comap [preorder β] (f : α → β) : preorder α :=
-{ le := λ x y, f x ≤ f y,
-  le_refl := λ x, le_refl (f x),
-  le_trans := λ x y z, le_trans (f x) (f y) (f z)}
+--def comap [preorder β] (f : α → β) : preorder α :=
+--{ le := λ x y, f x ≤ f y,
+--  le_refl := λ x, le_refl (f x),
+--  le_trans := λ x y z, le_trans (f x) (f y) (f z)}
 
-def comap' : preorder β → (α → β) → preorder α := @comap _ _
+/-- version of preorder.lift which doesn't use type class inference -/
+def lift' : preorder β → (α → β) → preorder α := @preorder.lift _ _
 
 end preorder
