9806bfb9345ba8ec1c777dd6fd7a3d9a5ebfc347
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2019-03-22 02:22:49 +0000

    tidying up

--- src/valuation_spectrum.lean
+++ src/valuation_spectrum.lean
@@ -7,16 +7,16 @@ import valuation.canonical
 The API for the valuation spectrum of a commutative ring. Normally defined as
 "the equivalence classes of valuations", there are set-theoretic issues.
 These issues are easily solved by noting that two valuations are equivalent
-if and only if they induce the same relation on R, where the relation
+if and only if they induce the same preorder on R, where the preorder
 attacted to a valuation sends (r,s) to v r ≤ v s.
 
 Our definition of Spv is currently the predicates which come from a
 valuation. There is another approach though: Prop 2.20 (p16) of
 https://homepages.uni-regensburg.de/~maf55605/contin_valuation.pdf
 classifies the relations which come from valuations as those satisfying
-some axioms. Here's why such a theorem must exist: given a relation coming
-from a valuation, we can reconstruct the support of the valuation (v r ≤ v 0),
-the relation on R / support coming from `on_quot v`, the relation on
+some axioms. See also Wedhorn 4.7. Here's why such a theorem must exist: given
+a relation coming from a valuation, we can reconstruct the support of the
+valuation (v r ≤ v 0), the relation on R / support coming from `on_quot v`, the relation on
 Frac(R/supp) coming from `on_frac v`, the things of valuation 1 in this
 field, and hence the value group of the valuation. The induced canonical
 valuation is a valuation we seek. This argument only uses a finite number of
@@ -34,6 +34,9 @@ computable, but `lift` often is.
 
 universes u u₀ u₁ u₂ u₃
 
+/-- Valuation spectrum of a ring. -/
+-- Note that the valuation takes values in a group in the same universe as R.
+-- This is to avoid "set-theoretic issues".
 definition Spv (R : Type u₀) [comm_ring R] :=
 {ineq : R → R → Prop // ∃ {Γ₀ : Type u₀} [linear_ordered_comm_group Γ₀],
  by exactI ∃ (v : valuation R Γ₀), ∀ r s : R, v r ≤ v s ↔ ineq r s}
@@ -51,6 +54,8 @@ variables {Γ₁ : Type u₁} [linear_ordered_comm_group Γ₁]
 variables {Γ₂ : Type u₂} [linear_ordered_comm_group Γ₂]
 
 -- The work is embedded here with `canonical_valuation_is_equiv v` etc.
+-- The canonical valuation attached to v lives in R's universe.
+/-- The constructor for a term of type Spv R given an arbitrary valuation -/
 definition mk (v : valuation R Γ) : Spv R :=
 ⟨λ r s, v r ≤ v s,
   ⟨value_group v, by apply_instance, canonical_valuation v, canonical_valuation_is_equiv v⟩⟩
@@ -58,13 +63,16 @@ definition mk (v : valuation R Γ) : Spv R :=
 @[simp] lemma mk_val (v : valuation R Γ) : (mk v).val = λ r s, v r ≤ v s := rfl
 
 -- This definition uses choice. We could avoid it if we used the 2.20 trick.
+/-- The value group attached to a term of type Spv R -/
 definition out_Γ (v : Spv R) : Type u₀ := classical.some v.2
 
 -- This instance could be made computable following the 2.20 trick.
 noncomputable instance (v : Spv R) : linear_ordered_comm_group (out_Γ v) :=
 classical.some $ classical.some_spec v.2
 
--- This instance could be made computable following the 2.20 trick
+-- This instance could be made computable following the 2.20 trick.
+-- Mario Carneiro points out that `out` is often noncomputable though.
+/-- An explicit valuation attached to a term of type Spv R -/
 noncomputable definition out (v : Spv R) : valuation R (out_Γ v) :=
 classical.some $ classical.some_spec $ classical.some_spec v.2
 
@@ -84,11 +92,13 @@ noncomputable def lift {X}
   (f : Π ⦃Γ₀ : Type u₀⦄ [linear_ordered_comm_group Γ₀], valuation R Γ₀ → X) (v : Spv R) : X :=
 f (out v)
 
+/-- The computation principle for Spv -/
 theorem lift_eq {X}
   (f₀ : Π ⦃Γ₀ : Type u₀⦄ [linear_ordered_comm_group Γ₀], valuation R Γ₀ → X)
   (f : Π ⦃Γ : Type u⦄ [linear_ordered_comm_group Γ], valuation R Γ → X)
   (v : valuation R Γ)
-  (h : ∀ ⦃Γ₀ : Type u₀⦄ [linear_ordered_comm_group Γ₀] (v₀ : valuation R Γ₀), v₀.is_equiv v → f₀ v₀ = f v) :
+  (h : ∀ ⦃Γ₀ : Type u₀⦄ [linear_ordered_comm_group Γ₀] (v₀ : valuation R Γ₀),
+    v₀.is_equiv v → f₀ v₀ = f v) :
   lift f₀ (mk v) = f v :=
 h _ (out_mk v)
 
@@ -128,6 +138,7 @@ section
 
 end
 
+/-- The open sets generating the topology of Spv R, see Wedhorn 4.1.-/
 definition basic_open (r s : R) : set (Spv R) :=
 {v | v r ≤ v s ∧ v s ≠ 0}
 
@@ -143,10 +154,6 @@ begin
 end
 
 end Spv
-
-
-
-
 
 #exit
 
