110b6e48e132b7d124fa33aa68676671a4c010e8
Patrick Massot <patrickmassot@free.fr>
2019-04-04 20:17:26 +0200

    Starts on topological field
    Endow units of a topological ring with a topological monoid structure
    Define topological division ring and prove its units form a top group.

---
+++ src/for_mathlib/topological_field.lean
@@ -0,0 +1,62 @@
+import tactic.converter.interactive
+import topology.algebra.ring
+
+open topological_space
+
+namespace topological_space
+variables {α : Type*} {β : Type*} {γ : Type*} {δ : Type*}
+variables [topological_space β] [topological_space δ]
+
+/-- A product of induced topologies is induced by the product map -/
+lemma prod_induced_induced (f : α → β) (g : γ → δ) :
+  @prod.topological_space _ _ (induced f ‹_›) (induced g ‹_›) = induced (λ p, (f p.1, g p.2)) prod.topological_space :=
+begin
+  letI := induced f ‹_›, letI := induced g ‹_›,
+  set fxg := (λ p : α × γ, (f p.1, g p.2)),
+  have key1 : f ∘ (prod.fst : α × γ → α) = (prod.fst : β × δ → β) ∘ fxg, from rfl,
+  have key2 : g ∘ (prod.snd : α × γ → γ) = (prod.snd : β × δ → δ) ∘ fxg, from rfl,
+  unfold prod.topological_space, -- product topology is a sup of induced topologies
+  -- so let's be functorial
+  conv_lhs {
+    rw [induced_compose, induced_compose, key1, key2],
+    congr, rw ← induced_compose, skip, rw ← induced_compose, },
+  rw induced_sup
+end
+end topological_space
+
+namespace topological_ring
+open topological_space
+variables {R : Type*} [ring R] [topological_space R]
+
+instance : topological_space (units R) := induced units.val ‹_›
+
+instance top_monoid_units [topological_ring R] : topological_monoid (units R) :=
+⟨begin
+  let mulR := (λ (p : R × R), p.1*p.2),
+  let mulRx := (λ (p : units R × units R), p.1*p.2),
+  have key : units.val ∘ mulRx = mulR ∘ (λ p, (p.1.val, p.2.val)), from rfl,
+  rw continuous_iff_induced_le,
+  unfold topological_ring.topological_space,
+  rw [induced_compose, key, ← induced_compose, prod_induced_induced],
+  apply induced_mono,
+  rw ← continuous_iff_induced_le,
+  exact continuous_mul',
+end⟩
+end topological_ring
+
+variables (K : Type*) [division_ring K] [topological_space K]
+
+/-- A topological division ring is a division ring with a topology where all operations are
+    continuous, including inversion. -/
+class topological_division_ring extends topological_ring K : Prop :=
+(continuous_inv : continuous (λa : units K, a⁻¹))
+
+namespace topological_division_ring
+
+variables [topological_division_ring K]
+
+instance units_top_group : topological_group (units K) :=
+{ continuous_inv := topological_division_ring.continuous_inv K,
+  ..topological_ring.top_monoid_units }
+
+end topological_division_ring
