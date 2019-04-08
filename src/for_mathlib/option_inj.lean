5477cdd0599995066b4f6fa35c0b0afa5e91d9a9
kckennylau <kc_kennylau@yahoo.com.hk>
2019-04-08 21:26:34 +0800

    cleanup option.map_inj

--- src/for_mathlib/option_inj.lean
+++ src/for_mathlib/option_inj.lean
@@ -4,13 +4,6 @@ open function
 
 theorem option.map_inj
   {α : Type u} {β : Type v} {f : α → β} (Hf : injective f) :
-injective (option.map f) := λ a₁ a₂ H,
-begin
-  cases h₁ : a₁; cases h₂ : a₂;
-  rw [h₁,h₂] at H;
-  try {exact option.no_confusion H},
-  { congr,
-    apply Hf,
-    apply option.some.inj,
-    exact H }
-end
+  injective (option.map f)
+| none      none      H := rfl
+| (some a₁) (some a₂) H := by rw Hf (option.some.inj H)
