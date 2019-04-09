1a813b2123a2883bc2778d3327cb739e83d50fe2
Scott Morrison <scott@tqft.net>
2019-04-10 00:57:53 +1000

    lots of sorries

--- src/not_exactly_an_adic_space_2.lean
+++ src/not_exactly_an_adic_space_2.lean
@@ -17,6 +17,7 @@ universes u v
 open algebraic_geometry
 open category_theory
 open category_theory.instances
+open category_theory.limits
 
 namespace algebraic_geometry.PresheafedSpace
 -- We define a shorthand for the stalk at a point, computed in Types
@@ -42,3 +43,57 @@ structure hom (F G : whatsit.{v}) :=
 (hom : F.to_PresheafedSpace ⟶ G.to_PresheafedSpace)
 (monotone : Π (x : F.X) (a b : G.to_PresheafedSpace.stalk' (PresheafedSpace.hom.f hom x)),
    (a ≤ b) ↔ ((stalk_map' hom x) a ≤ (stalk_map' hom x) b))
+.
+
+-- FIXME can't tag this with @[extensionality]?
+lemma hom.ext {F G : whatsit.{v}} {f g : hom F G} (w : f.hom = g.hom) : f = g :=
+begin
+  cases f, cases g,
+  congr; assumption
+end
+
+
+-- We need two lemmas about `stalk_map'`:
+section
+
+@[simp] lemma stalk_map'_id (F : PresheafedSpace.{v} TopCommRing.{v}) (x : F.X) :
+  stalk_map' (𝟙 F) x = 𝟙 (F.stalk' x) :=
+begin
+  dsimp [stalk_map', stalk'],
+  -- because of tangled type dependencies, we're going to have to give the original proof all over again
+  sorry
+end
+@[simp] lemma stalk_map'_comp {F G H : PresheafedSpace.{v} TopCommRing.{v}} (α : F ⟶ G) (β : G ⟶ H) (x : F.X) :
+  stalk_map' (α ≫ β) x =
+    begin
+      have p := (stalk_map' β (α x) : H.stalk' (β (α x)) ⟶ G.stalk' (α x)),
+      have q := (stalk_map' α x : G.stalk' (α x) ⟶ F.stalk' x),
+      exact q ∘ p
+    end :=
+sorry
+end
+
+def hom.id (F : whatsit.{v}) : hom F F :=
+{ hom := 𝟙 F.to_PresheafedSpace,
+  monotone := λ x a b, by simp,  }
+
+def hom.comp (F G H : whatsit.{v}) (α : hom F G) (β : hom G H) : hom F H :=
+{ hom := α.hom ≫ β.hom,
+  monotone := λ x a b,
+  begin
+    simp,
+    transitivity,
+    apply β.monotone,
+    apply α.monotone,
+  end  }
+
+section
+local attribute [simp] id comp
+instance : category whatsit.{v} :=
+{ hom := hom,
+  id := hom.id,
+  comp := hom.comp,
+  comp_id' := λ X Y f, sorry,
+  id_comp' := λ X Y f, sorry,
+  assoc' := λ W X Y Z f g h, sorry }
+end
