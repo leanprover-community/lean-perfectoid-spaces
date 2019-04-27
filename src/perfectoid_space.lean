2377801c92ef9a343afc8375b45775cb52c45bc5
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2019-04-27 21:51:50 +0100

    some attempt at beautification

--- src/perfectoid_space.lean
+++ src/perfectoid_space.lean
@@ -20,9 +20,10 @@ class perfectoid_ring (R : Type u) [Huber_ring R] extends Tate_ring R : Prop :=
 (ramified : ∃ ϖ : pseudo_uniformizer R, (ϖ^p : Rᵒ) ∣ p)
 (Frob     : ∀ a : Rᵒ, ∃ b : Rᵒ, (p : Rᵒ) ∣ (b^p - a : Rᵒ))
 
+-- 𝒞 is a category whose objects are topological spaces with a presheaf of topological rings and
+-- and an equivalence class of valuation on each stalk; a perfectoid space is locally
+-- isomorphic to Spa(A) with A a perfectoid ring, and the isomorphism can be checked in 𝒞.
+
 class perfectoid_space (X : Type u) [topological_space X] extends adic_space X :=
 (perfectoid_cover : ∀ x : X, ∃ (U : opens X) (A : Huber_pair) [perfectoid_ring A],
-  -- next hypothesis is always true although we're yet to prove this
-  (x ∈ U) ∧ nonempty (𝒞.equiv (𝒞.Spa A : 𝒞 (Spa A))
-    ((locally_ringed_valued_space).to_𝒞.restrict U : 𝒞 U)))
--- is_preadic_space_equiv U (Spa A))
+  (x ∈ U) ∧ (𝒞.Spa A) ≅_𝒞 (locally_ringed_valued_space.to_𝒞.restrict U))
