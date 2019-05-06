2d8adae25a5518a5957e8cc0855366d340a55f06
Johan M. Commelin <johan@commelin.net>
2019-05-06 19:42:17 +0200

    Remove for files that have been merged into mathlib

--- src/for_mathlib/logic.lean
+++
@@ -1,6 +0,0 @@
-import logic.basic
-
--- PR'd as #977.
-theorem forall_iff_forall_surj {α β : Type*} {f : α → β} (h : function.surjective f) {P : β → Prop} :
-(∀ a, P (f a)) ↔ ∀ b, P b:=
-⟨λ ha b, by cases h b with a hab; rw ←hab; exact ha a, λ hb a, hb $ f a⟩
