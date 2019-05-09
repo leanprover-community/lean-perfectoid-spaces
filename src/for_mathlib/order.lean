6cd864f941112004a37cea5bbe10697ad810493f
Johan Commelin <johan@commelin.net>
2019-05-09 08:59:18 +0200

    This is now in mathlib

--- src/for_mathlib/order.lean
+++
@@ -1,9 +0,0 @@
-import order.basic
-namespace preorder
-
-variables {α : Type*} {β : Type*}
-
-/-- version of preorder.lift which doesn't use type class inference -/
-def lift' : preorder β → (α → β) → preorder α := @preorder.lift _ _
-
-end preorder
