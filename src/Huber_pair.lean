e89d448edb00e8e6784c45224e47290282892764
Johan Commelin <johan@commelin.net>
2019-05-21 11:36:49 +0200

    Add docstring, remove dead code

--- src/Huber_pair.lean
+++ src/Huber_pair.lean
@@ -10,9 +10,6 @@ open power_bounded
 section integral
 variables {R : Type u} [comm_ring R] [decidable_eq R]
 
--- instance subtype.val.is_ring_hom (S : set R) [is_subring S] : is_ring_hom (@subtype.val _ S) :=
--- by apply_instance
-
 def is_integral (S : set R) [is_subring S] (r : R) : Prop :=
 ∃ f : polynomial ↥S, (f.monic) ∧ f.eval₂ (@subtype.val _ S) r = 0
 
@@ -30,17 +27,23 @@ structure is_ring_of_integral_elements {R : Type u}
 (is_power_bounded : Rplus ⊆ Rᵒ)
 
 -- a Huber Ring is an f-adic ring.
--- a Huber Pair is what Huber called an Affinoid Ring.
+/-- A Huber pair consists of a Huber ring and a
+so-call ring of integral elements: an integrally closed, power bounded, open subring.
+(Huber called such objects “affinoid rings”.) -/
 structure Huber_pair :=
 (R : Type) -- change this to (Type u) to enable universes
 [RHuber : Huber_ring R]
 [dec : decidable_eq R]
 (Rplus : set R)
-[intel : is_ring_of_integral_elements Rplus]
+(intel : is_ring_of_integral_elements Rplus)
+
+namespace Huber_pair
 
 instance : has_coe_to_sort Huber_pair :=
 { S := Type, coe := Huber_pair.R }
 
-instance Huber_pair.Huber_ring (A : Huber_pair) : Huber_ring A := A.RHuber
+instance (A : Huber_pair) : Huber_ring A := A.RHuber
+
+end Huber_pair
 
 postfix `⁺` : 66 := λ A : Huber_pair, A.Rplus
