7f5b2148772dfe4018276b6d1a8783e858b4f583
Johan Commelin <johan@commelin.net>
2019-07-05 10:31:46 +0200

    Weird stuff with primes

--- src/prime.lean
+++ src/prime.lean
@@ -1,10 +1,13 @@
 import algebra.char_p
 import ring_theory.ideals
 
+-- I think this paragraph should be removable. BUt Lean doesn't agree.
 -- The next two lines have been PR'd to mathlib in #1073
-def Prime := { p : ℕ // nat.prime p}
+def Prime := { p : ℕ // p.prime }
 instance : has_coe Prime ℕ := ⟨subtype.val⟩
-instance monoid.prime_pow {α : Type*} [monoid α]: has_pow α Prime := ⟨λ x p, x^p.val⟩
+instance monoid.Prime_pow {α : Type*} [monoid α]: has_pow α Prime := ⟨λ x p, x^p.val⟩
+
+instance monoid.prime_pow {α : Type*} [monoid α]: has_pow α nat.primes := ⟨λ x p, x^p.val⟩
 
 noncomputable def Frobenius (α : Type*) [semiring α] : α → α := λ x, x^(ring_char α)
 notation `Frob` R `∕` x := Frobenius (ideal.quotient (ideal.span ({x} : set R)))
