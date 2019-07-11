047bc5304d8367502e85640024284f01d235a47a
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2019-07-11 01:21:10 +0100

    removing some sorrys

--- src/perfectoid_space_examples.lean
+++ src/perfectoid_space_examples.lean
@@ -17,13 +17,15 @@ def empty_CLVRS : CLVRS := {
   top := ⊤,
   sheaf := { F := { F := λ _, unit,
   res := λ _ _ _ _, (),
-  Hid := sorry,
-  Hcomp := sorry,
+  Hid := λ U, by {funext x, cases x, refl},
+  Hcomp := λ U V W _ _, rfl,
   Fring := λ x, punit.comm_ring,
-  res_is_ring_hom := sorry,
+  res_is_ring_hom := λ U V _, { map_one := _,
+  map_mul := λ  _ _, rfl,
+  map_add := λ _ _, rfl,
   Ftop := λ U, by apply_instance,
   Ftop_ring := λ U, by apply_instance,
-  res_continuous := sorry },
+  res_continuous := continuous_of_discrete_topology },
   locality := sorry,
   gluing := sorry,
   homeo := sorry },
@@ -31,3 +33,13 @@ def empty_CLVRS : CLVRS := {
   valuation := by rintro ⟨⟩,
   local_stalks := by rintro ⟨⟩,
   supp_maximal := by rintro ⟨⟩ }
+}
+
+example : PerfectoidSpace ⟨37, by norm_num⟩ := ⟨empty_CLVRS, by rintro ⟨⟩⟩
+
+#exit
+begin
+  unfold is_perfectoid,
+  intro x,
+  cases x,
+end⟩
