99a9f18b2a251d9f934c8e67229e11941b75d1d5
Johan Commelin <johan@commelin.net>
2019-08-30 16:42:53 +0200

    Add comments to Frobenius.lean

--- src/Frobenius.lean
+++ src/Frobenius.lean
@@ -1,6 +1,14 @@
 import algebra.char_p
 import for_mathlib.primes
 
+/-!
+The main purpose of this file is to introduce notation that
+is not available in mathlib, and that we don't want to set up in the main file.
+-/
+
+/--The Frobenius endomorphism of a semiring-/
 noncomputable def Frobenius (α : Type*) [semiring α] : α → α := λ x, x^(ring_char α)
+
 notation `Frob` R `∕` x := Frobenius (ideal.quotient (ideal.span ({x} : set R)))
+
 notation x `∣` y `in` R := (x : R) ∣ (y : R)
