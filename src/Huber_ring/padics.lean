adb874d55f687edf3745f09162f4614487301a35
Johan Commelin <johan@commelin.net>
2019-09-21 09:42:23 +0200

    Little progress

--- src/Huber_ring/padics.lean
+++ src/Huber_ring/padics.lean
@@ -1,9 +1,75 @@
 import data.padics
 import Huber_ring.basic
 
-variables (p : ℕ) [nat.prime p]
+noncomputable theory
+open_locale classical
+
+variables {p : ℕ} [nat.prime p]
 
 local attribute [-simp] padic.cast_eq_of_rat_of_nat
+
+-- This should be generalised in 10 directions
+lemma rat.fpow_strict_mono {x : ℚ} (hx : 1 < x) :
+  strict_mono (λ n:ℤ, x ^ n) :=
+λ m n h, show x ^ m < x ^ n,
+begin
+  have xpos : 0 < x := by linarith,
+  have h₀ : x ≠ 0 := by linarith,
+  have hxm : 0 < x^m := fpow_pos_of_pos xpos m,
+  have hxm₀ : x^m ≠ 0 := ne_of_gt hxm,
+  suffices : 1 < x^(n-m),
+  { replace := mul_lt_mul_of_pos_right this hxm,
+    simpa [*, fpow_add, mul_assoc, fpow_neg, inv_mul_cancel], },
+  apply one_lt_fpow hx, linarith,
+end
+
+-- This should be generalised in 10 directions
+@[simp] lemma rat.fpow_mono {x : ℚ} (hx : 1 < x) {m n : ℤ} :
+  x ^ m < x ^ n ↔ m < n :=
+(rat.fpow_strict_mono hx).lt_iff_lt
+
+-- This should be generalised in 10 directions
+@[simp] lemma rat.fpow_inj'' {x : ℚ} (h₀ : 0 < x) (h₁ : x ≠ 1) {m n : ℤ} :
+  x ^ m = x ^ n ↔ m = n :=
+begin
+  split; intro h, swap, {simp [h]},
+  rcases lt_trichotomy x 1 with H|rfl|H,
+  { apply (rat.fpow_strict_mono (one_lt_inv h₀ H)).injective,
+    show x⁻¹ ^ m = x⁻¹ ^ n,
+    rw [← fpow_inv, ← fpow_mul, ← fpow_mul, mul_comm _ m, mul_comm _ n, fpow_mul, fpow_mul, h], },
+  { contradiction },
+  { exact (rat.fpow_strict_mono H).injective h, },
+end
+
+namespace padic_seq
+
+def valuation (f : padic_seq p) : ℤ :=
+if hf : f ≈ 0 then 0 else padic_val_rat p (f (stationary_point hf))
+
+lemma norm_eq_pow_val {f : padic_seq p} (hf : ¬ f ≈ 0) :
+  f.norm = p^(-f.valuation : ℤ) :=
+begin
+  delta norm valuation,
+  rw [dif_neg hf, dif_neg hf],
+  delta padic_norm,
+  rw if_neg,
+  assume H, apply cau_seq.not_lim_zero_of_not_congr_zero hf,
+  intros ε hε,
+  use (stationary_point hf),
+  intros n hn,
+  rw stationary_point_spec hf (le_refl _) hn,
+  simpa [H] using hε,
+end
+
+lemma val_eq_iff_norm_eq {f g : padic_seq p} (hf : ¬ f ≈ 0) (hg : ¬ g ≈ 0) :
+  f.valuation = g.valuation ↔ f.norm = g.norm :=
+begin
+  rw [norm_eq_pow_val hf, norm_eq_pow_val hg, ← neg_inj', rat.fpow_inj''],
+  { exact_mod_cast nat.prime.pos ‹_› },
+  { exact_mod_cast nat.prime.ne_one ‹_› },
+end
+
+end padic_seq
 
 namespace padic_rat
 
@@ -19,6 +85,13 @@ end
 @[simp] lemma norm_p_pow (n : ℤ) : ∥(p^n : ℚ_[p])∥ = p^-n :=
 by rw [norm_fpow, norm_p, fpow_neg, one_div_eq_inv,
   ← fpow_inv, ← fpow_inv, ← fpow_mul, ← fpow_mul, mul_comm]
+
+def valuation : ℚ_[p] → ℤ :=
+quotient.lift (padic_seq.valuation) $ λ f g h,
+begin
+  sorry
+  -- by_cases hf : f ≈ 0,
+end
 
 end padic_rat
 
@@ -80,7 +153,7 @@ instance coe_is_ring_hom : is_ring_hom (coe : ℤ_[p] → ℚ_[p]) :=
   map_mul := coe_mul,
   map_add := coe_add }
 
-noncomputable def algebra : algebra ℤ_[p] ℚ_[p] :=
+def algebra : algebra ℤ_[p] ℚ_[p] :=
 @algebra.of_ring_hom ℤ_[p] _ _ _ (coe) padic_int.coe_is_ring_hom
 
 lemma aux (p : ℚ) (n : ℤ) (hp : 1 ≤ p) (h : p ^ n < p) : p ^ n ≤ 1 :=
@@ -109,7 +182,7 @@ lemma coe_open_embedding : open_embedding (coe : ℤ_[p] → ℚ_[p]) :=
 
 variable (p)
 
-noncomputable def Huber_ring : Huber_ring ℤ_[p] :=
+def Huber_ring : Huber_ring ℤ_[p] :=
 { pod := ⟨ℤ_[p], infer_instance, infer_instance, by apply_instance,
   ⟨{ emb := open_embedding_id,
     J := (nonunits_ideal _),
@@ -122,7 +195,7 @@ end padic_int
 namespace padic_rat
 open local_ring padic_int
 
-noncomputable def Huber_ring : Huber_ring ℚ_[p] :=
+def Huber_ring : Huber_ring ℚ_[p] :=
 { pod := ⟨ℤ_[p], infer_instance, infer_instance, by apply_instance,
   ⟨{ emb := padic_int.coe_open_embedding,
     J := (nonunits_ideal _),
