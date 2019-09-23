9f11d8c1d2cd5eba58a4a387473b69112238ceee
Johan Commelin <johan@commelin.net>
2019-09-23 16:32:56 +0200

    Padics form a Huber ring (#55)

---
+++ src/Huber_ring/padics.lean
@@ -0,0 +1,63 @@
+import data.padics
+import Huber_ring.basic
+import for_mathlib.field_power
+import for_mathlib.ideal_operations
+import for_mathlib.padics
+
+noncomputable theory
+
+open local_ring
+
+variables (p : ℕ) [nat.prime p]
+namespace padic_int
+lemma is_adic : is_ideal_adic (nonunits_ideal ℤ_[p]) :=
+begin
+  rw is_ideal_adic_iff,
+  split,
+  { intro n,
+    show is_open (↑(_ : ideal ℤ_[p]) : set ℤ_[p]),
+    rw power_nonunits_ideal_eq_norm_le_pow,
+    simp only [norm_le_pow_iff_norm_lt_pow_succ],
+    rw ← ball_0_eq,
+    exact metric.is_open_ball },
+  { intros s hs,
+    rcases metric.mem_nhds_iff.mp hs with ⟨ε, ε_pos, hε⟩,
+    obtain ⟨n, hn⟩ : ∃ n : ℕ, (p : ℝ)^-(n:ℤ) < ε,
+    { have hp : (1:ℝ) < p := by exact_mod_cast nat.prime.one_lt ‹_›,
+      obtain ⟨n, hn⟩ : ∃ (n:ℕ), ε⁻¹ < p^n := pow_unbounded_of_one_lt ε⁻¹ hp,
+      use n,
+      have hp' : (0:ℝ) < p^n,
+      { rw ← fpow_of_nat, apply fpow_pos_of_pos, exact_mod_cast nat.prime.pos ‹_› },
+      rw [inv_lt ε_pos hp', inv_eq_one_div] at hn,
+      rwa [fpow_neg, fpow_of_nat], },
+    use n, show (↑(_ : ideal ℤ_[p]) : set ℤ_[p]) ⊆ _,
+    refine set.subset.trans _ hε,
+    rw power_nonunits_ideal_eq_norm_le_pow,
+    rw ball_0_eq,
+    intros x hx,
+    rw set.mem_set_of_eq at *,
+    exact lt_of_le_of_lt hx hn }
+end
+
+def Huber_ring : Huber_ring ℤ_[p] :=
+{ pod := ⟨ℤ_[p], infer_instance, infer_instance, by apply_instance,
+  ⟨{ emb := open_embedding_id,
+    J := (nonunits_ideal _),
+    fin := nonunits_ideal_fg p,
+    top := is_adic p,
+    .. algebra.id ℤ_[p] }⟩⟩ }
+
+end padic_int
+
+namespace padic
+open local_ring padic_int
+
+def Huber_ring : Huber_ring ℚ_[p] :=
+{ pod := ⟨ℤ_[p], infer_instance, infer_instance, by apply_instance,
+  ⟨{ emb := padic_int.coe_open_embedding,
+    J := (nonunits_ideal _),
+    fin := nonunits_ideal_fg p,
+    top := is_adic p,
+    .. padic_int.algebra }⟩⟩ }
+
+end padic
