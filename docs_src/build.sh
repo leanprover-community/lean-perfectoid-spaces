43954f64ce4afdf4fa39e2aeef8ec43826b24f2a
Patrick Massot <patrickmassot@free.fr>
2020-05-03 11:55:20 +0200

    Try again md+TeX

--- docs_src/build.sh
+++ docs_src/build.sh
@@ -1,4 +1,4 @@
 for i in `ls *.md` 
 do 
-	pandoc -t html --mathjax -f markdown+tex_math_dollars-raw_tex --filter ./pandocfilter-pygments $i --template template.html -o ../docs/"${i%.md}.html"
+	pandoc -t html --mathjax -f markdown+tex_math_dollars+raw_tex --filter ./pandocfilter-pygments $i --template template.html -o ../docs/"${i%.md}.html"
 done
