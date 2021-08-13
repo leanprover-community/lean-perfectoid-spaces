c279c6286974b8ad97bfc01589cb64aa4534cf65
Patrick Massot <patrickmassot@free.fr>
2021-08-14 01:57:30 +0200

    Remove unused files

--- src/for_mathlib/cardinal.lean
+++
@@ -1,6 +0,0 @@
-import set_theory.cardinal
-
-lemma cardinal.eq_one_iff_nonempty_subsingleton {α : Type*} :
-  cardinal.mk α = 1 ↔ (nonempty α ∧ subsingleton α) :=
-by rw [← cardinal.ne_zero_iff_nonempty, ← cardinal.le_one_iff_subsingleton,
-  eq_iff_le_not_lt, ← cardinal.succ_zero, cardinal.lt_succ, cardinal.le_zero, and_comm]
